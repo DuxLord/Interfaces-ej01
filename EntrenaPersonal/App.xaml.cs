57cd0ca533f9b9828181ec1950ea95bec73c267e
DuxLord <gcardonamedina@gmail.com>
2024-10-21 19:11:27 +0200

    Agregar archivos de proyecto.

---
+++ EntrenaPersonal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EntrenaPersonal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EntrenaPersonal/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EntrenaPersonal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
